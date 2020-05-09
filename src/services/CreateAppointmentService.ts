bc23660a99491b1327add195518838e490f5373a
Luis Coelho <luis.fs.coelho@pm.me>
2020-05-08 22:15:36 -0300

    Tratando exceções

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import AppError from '../errors/AppError';
 
 interface Request {
   provider_id: string;
@@ -19,7 +20,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentInSameDate) {
-      throw Error('This aappointment is alredy booked');
+      throw new AppError('This aappointment is alredy booked');
     }
     const appointment = appointmentsRepository.create({
       provider_id,
