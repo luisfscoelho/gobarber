5ae744f1cf473dff547d992fa7330f23a22e6f53
Luis Coelho <luis.fs.coelho@pm.me>
2020-05-08 21:20:13 -0300

    upload de imagens

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,6 +4,7 @@ import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -33,7 +34,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.json({});
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   },
 );
 
---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,40 @@
+import path from 'path';
+import fs from 'fs';
+import { getRepository } from 'typeorm';
+
+import uploadConfig from '../config/upload';
+import User from '../models/User';
+
+interface Request {
+  user_id: string;
+  avatarFileName: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFileName }: Request): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar');
+    }
+
+    if (user.avatar) {
+      const userAvatarPath = path.resolve(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarPath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarPath);
+      }
+    }
+
+    user.avatar = avatarFileName;
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
