8956dd03f9190a38cb71e34916d7c5248f4e7425
Luis Coelho <luis.fs.coelho@pm.me>
2020-06-16 21:02:44 -0300

    Reset de senha

--- src/modules/users/repositories/IUserTokensRepository.ts
+++ src/modules/users/repositories/IUserTokensRepository.ts
@@ -2,4 +2,5 @@ import UserToken from '../infra/typeorm/entities/UserToken';
 
 export default interface IUserTokensInterface {
   generate(user_id: string): Promise<UserToken>;
+  findByToken(token: string): Promise<UserToken | undefined>;
 }
